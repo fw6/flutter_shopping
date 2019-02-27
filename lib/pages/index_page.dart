22070044194f2715a1a32ea925cd4db90f933d6f
fw6 <lyman.von9506@gmail.com>
2019-02-27 22:12:37 +0800

    feat: init

---
+++ lib/pages/index_page.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_shopping/pages/cart_page.dart';
+import 'package:flutter_shopping/pages/category_page.dart';
+import 'package:flutter_shopping/pages/member_page.dart';
+import 'package:flutter_shopping/pages/home_page.dart';
+
+class IndexPage extends StatefulWidget {
+  _IndexPageState createState() => _IndexPageState();
+}
+
+class _IndexPageState extends State<IndexPage> {
+  final List<BottomNavigationBarItem> bottomTabs = [
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.home),
+      title: Text('首页'),
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.search),
+      title: Text('分类'),
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.shopping_cart),
+      title: Text('购物车'),
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.profile_circled),
+      title: Text('个人中心'),
+    ),
+  ];
+
+  final List<Widget> tabBodies = [
+    HomePage(),
+    CategoryPage(),
+    CartPage(),
+    MemberPage()
+  ];
+
+  int _currentIndex = 0;
+  var _currentPage;
+
+  void initState() {
+    super.initState();
+    _currentPage = tabBodies[_currentIndex];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(244, 245, 245, 1.0),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _currentIndex,
+        items: bottomTabs,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+            _currentPage = tabBodies[_currentIndex];
+          });
+        },
+      ),
+      body: _currentPage,
+    );
+  }
+}
