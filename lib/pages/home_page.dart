3664833846ca8c4821b48e6c5e0ec79a29a60a59
fw6 <lyman.von9506@gmail.com>
2019-02-27 22:53:05 +0800

    dio

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    getHttp();
     return Scaffold(
       body: Center(
         child: Text('商城首页'),
       ),
     );
+  }
+
+  void getHttp() async {
+    try {
+      Response response;
+      response = await Dio().get(
+          'https://www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/dabaojian?name=大胸美女');
+      return print(response);
+    } catch (err) {
+      return print(err);
+    }
   }
 }
